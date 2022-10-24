b1d01a60bc1e2d1aceb9fdbff3895865fd213b3a
gkranasinghe <gkranasinghe@gmail.com>
2022-10-24 10:13:18 +0530

    initial commit

---
+++ orders/api/schemas.py
@@ -0,0 +1,36 @@
+from asyncore import dispatcher_with_send
+from cgitb import small
+from enum import Enum
+import imp
+from typing import List
+from uuid import UUID
+from click import progressbar
+from numpy import size
+
+from pydantic import BaseModel,Field
+
+class SizeEnum(str, Enum):
+    small   = "small"
+    medium = "medium"
+    big = "big"
+
+class StatusEnum(str, Enum):
+    created = "created"
+    progress = "progress"
+    cancelled = "cancelled"
+    dispatched = "dispatched"
+    delivered = "delivered"
+
+class OrderItemSchema(BaseModel):
+    product: str
+    size: SizeEnum
+    quantity: int = Field(default="1", ge="1")
+
+class CreateOrderSchema(BaseModel):
+    order:List[OrderItemSchema]
+
+class GetOrderSchema(CreateOrderSchema):
+    id:UUID
+    created: int = Field(description="Date in the form of unix timestamp")
+    status:StatusEnum
+
